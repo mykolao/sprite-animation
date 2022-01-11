fff9ce420657cd883b4ad8e0fe92dd99d8b54781
mykolao <mykolaoleskiv@gmail.com>
2022-01-11 08:40:07 +0200

    successful sprite render

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,33 @@
-const hello = document.createElement("h1");
-hello.textContent = "Hello Typescript ❤️";
-hello.style.color = "white";
-hello.style.fontSize = "4rem";
-document.querySelector(".container")?.appendChild(hello);
+import idleSpriteImg from "./sprites/idle.png";
+
+const canvas = document.createElement("canvas");
+canvas.width = canvas.height = 150;
+const ctx = canvas.getContext("2d");
+
+const idleSpriteSheet = document.createElement("img");
+idleSpriteSheet.src = idleSpriteImg;
+
+const spriteSize = {
+  w: 119,
+  h: 124,
+};
+
+const root =
+  document.querySelector(".root") ||
+  document.createElement("div");
+
+root.appendChild(canvas);
+
+idleSpriteSheet.onload = () => {
+  ctx?.drawImage(
+    idleSpriteSheet,
+    0,
+    0,
+    spriteSize.w,
+    spriteSize.h,
+    0,
+    0,
+    spriteSize.w,
+    spriteSize.h
+  );
+};
