44d0b14578ba13c2c2110748ca1be22a51d0f4cd
Mykola <57754283+mykolao@users.noreply.github.com>
2022-01-11 07:51:14 +0200

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+const hello = document.createElement("h1");
+hello.textContent = "Hello Typescript ❤️";
+hello.style.color = "white";
+hello.style.fontSize = "4rem";
+document.querySelector(".container")?.appendChild(hello);
